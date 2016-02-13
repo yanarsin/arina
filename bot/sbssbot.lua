85ff3cbba493a3105b994a41d99f55576242aaef
yanarsin <lfrshydfr@gmail.com>
2016-02-13 10:14:34 +0000

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -232,7 +232,7 @@ function create_config( )
     "id",
     "tagall"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {163354254,172268740},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
